318ea5935155f28698bf0b662a90d63c776c2e53
Igor Lyalyushkin <61538902+igorL-JS@users.noreply.github.com>
2020-08-02 12:05:08 +0300

    Updates

---
+++ precache-manifest.4c1556ce16429c7d1660a706bb015443.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f5da7958c0ea47324a158cca164d3f99",
+    "url": "/Delivery/index.html"
+  },
+  {
+    "revision": "10011490fc77168cb6b3",
+    "url": "/Delivery/static/css/main.fe240c78.chunk.css"
+  },
+  {
+    "revision": "c9fe509a6fc0637c8958",
+    "url": "/Delivery/static/js/2.2b012039.chunk.js"
+  },
+  {
+    "revision": "77e7f328dacc981fca681ee4ae94d669",
+    "url": "/Delivery/static/js/2.2b012039.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "10011490fc77168cb6b3",
+    "url": "/Delivery/static/js/main.645eaafd.chunk.js"
+  },
+  {
+    "revision": "0f4785841bae5621c180",
+    "url": "/Delivery/static/js/runtime-main.5be558a2.js"
+  },
+  {
+    "revision": "f3478393e7681500a32f2bf03e1bb032",
+    "url": "/Delivery/static/media/Vector.f3478393.svg"
+  }
+]);
