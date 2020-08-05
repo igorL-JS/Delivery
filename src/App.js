60a97867fc9c72ce5d6ad38e01d996f8e2bac597
Igor Lyalyushkin <61538902+igorL-JS@users.noreply.github.com>
2020-08-05 21:58:18 +0300

    final

--- src/App.js
+++ src/App.js
@@ -4,23 +4,25 @@ import {Pickup} from "./Components/Pickup";
 import {YMaps} from "react-yandex-maps";
 import {Delivery} from "./Components/Delivery";
 
+
 const App = () => {
 
+debugger
     return (
       <div className="container">
           <div className="content">
               <header className="title">
                   Выберите способ доставки
               </header>
-                <input type="radio" id="tab1" name="tabs" className="radiobutton" checked/>
-                <input type="radio" id="tab2" name="tabs" className="radiobutton"/>
+
+                <input  type="radio" id="tab1" name="tabs" className="radiobutton" checked />
+                <input type="radio" id="tab2" name="tabs" className="radiobutton" />
+
                 <nav className="tabs">
-                    <div className="tabs__el tab1 "><label htmlFor="tab1" className="tabs__title">Доставка</label></div>
-                    <div className="tabs__el tab2"><label htmlFor="tab2" className="tabs__title">Самовывоз</label></div>
+                    <label htmlFor="tab1" className="tabs__el tab1"><span className="tabs__title">Доставка</span></label>
+                    <label htmlFor="tab2" className="tabs__el tab2"><span className="tabs__title">Самовывоз</span></label>
                 </nav>
-
                     <Delivery/>
-
                     <YMaps>
                         <Pickup/>
                     </YMaps>
